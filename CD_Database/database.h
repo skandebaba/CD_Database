4beaab0891be9beefe55949103b36d0b56a532fd
skandebaba <johann.padkos@gmail.com>
2013-09-05 15:07:50 +1200

    More Minor Improvements
    Added database.h which contain all the #define.
    No more 'magic numbers' in code.
    Added ifndef NOARTIST, so if we include #define NOARTIST, program will
    not ask you to enter the artist.

---
+++ CD_Database/database.h
@@ -0,0 +1,11 @@
+/*
+ *  database.h
+ *
+ *  Header file for use with main.c
+ *
+ */
+
+#define     MAX_CDS          100
+#define     ARTIST_SIZE      60     // This is not including the null character
+#define     TITLE_SIZE       60     // This is not including the null character
+// #define     NOARTIST                // Comment out if you want no artist
--- CD_Database/main.c
+++ CD_Database/main.c
@@ -5,17 +5,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "database.h"
 
 int main()
 {
-    char    artist[100][61];         // Store the artist of the CD
-    char    title[100][61];          // Store the title of the CD
-    int     tracks[100];             // Store number of tracks on CD
-    char    type;                    // Used as a boolean
-    int     album[100];              // boolean - album or single?
-    float   price[100];              // Store the price of the CD
-    int     count = 0;               // How many CDs are being tracked
-    int     i;                       // Loop counter
+#ifndef NOARTIST
+    char    artist[MAX_CDS][ARTIST_SIZE+1];         // Store the artist of the CD
+#endif
+    char    title[MAX_CDS][TITLE_SIZE+1];       // Store the title of the CD
+    int     tracks[MAX_CDS];                        // Store number of tracks on CD
+    char    type;                                   // Used as a boolean
+    int     album[MAX_CDS];                         // boolean - album or single?
+    float   price[MAX_CDS];                         // Store the price of the CD
+    int     count = 0;                              // How many CDs are being tracked
+    int     i;                                      // Loop counter
 
     puts("Welcome to the CD database");
     printf("You can store a maximum of %d CDs", sizeof album/sizeof album[0]);
@@ -35,12 +38,15 @@ int main()
 
         puts("");       // newline - for neat output
 
-        // Prompt user to enter ARTIST of the CD
+
         printf("Please enter the details of CD %d...\n", count+1);
+#ifndef NOARTIST
+        // Prompt user to enter ARTIST of the CD
         fputs("Artist? ", stdout);
         fflush(stdin);
         fgets(artist[count], sizeof artist[count], stdin);
         artist[count][strlen(artist[count])-1] = '\0';  // Remove newline
+#endif
 
         // Prompt user to enter TITLE of the CD
         fputs("Title? ", stdout);
@@ -73,7 +79,7 @@ int main()
         scanf("%f", &price[count]);
 
         // Check if array has been filled up
-        if (++count == 100)
+        if (++count == MAX_CDS)
         {
             puts("You have reached the limits of this program\n");
             break;
@@ -85,7 +91,9 @@ int main()
         // Output the details of the CD
         printf("\nThe details you have entered for CD %d are:\n", i+1);
         puts("=========================================");
+#ifndef NOARTIST
         printf("Artist: %s\n", artist[i]);
+#endif
         printf("Title: %s\n", title[i]);
         printf("Number of tracks: %d\n", tracks[i]);
         puts(album[i] == 1 ? ("Album") : ("Single"));       // album[i] == 1 ? puts("Album") : puts("Single");
